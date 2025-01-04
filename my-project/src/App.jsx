588f2f6501630fb8ae6f39b8d48350924054960d
Siran-web <simranhaidar91@gmail.com>
2025-01-04 22:25:43 +0530

    Sound effect added :

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,23 +1,50 @@
 import bgImg from "./assets/oggyhome.webp";
 import bg from "./assets/mainBg.jpg";
 import CircleGrid from "./components/circle";
-import { useState} from "react";
+import audio from "./assets/startBg.mp3";
+import fail from "./assets/Fail.mp3";
+import { useState, useRef } from "react";
+import beat from "./assets/beat.mp3";
+import cursor from './assets/cursor.png';
 
 function App() {
-    const [point,setPoint] = useState(0);
-    const [gameOver,setGameOver] = useState(false);
-  
-    const handleClick = ()=>{
-      setPoint((prevPoint)=>prevPoint+5);
-    };
+  const [point, setPoint] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  const [start, setStart] = useState(false);
+  const audioRef = useRef(null);
+  const failRef = useRef(null);
+  const beatRef = useRef(null);
 
-    const handleClickOggy = ()=>{
-      setGameOver(true);
+  const startGame = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
     }
+    setGameOver(false);
+    setStart(true);
+    setPoint(0);
+  };
+
+  const handleClick = () => {
+    setPoint((prevPoint) => prevPoint + 5);
+    if (beatRef.current) {
+      beatRef.current.currentTime = 0;
+      beatRef.current.play();
+    }
+  };
+
+  const handleClickOggy = () => {
+    setGameOver(true);
+    if (failRef.current) {
+      failRef.current.play();
+    }
+    if (audioRef.current) {
+      audioRef.current.pause();
+    }
+  };
 
   return (
     <>
-      <div className="w-full h-screen bg-slate-600">
+      <div className="w-full h-screen font-serif bg-slate-600">
         <div
           className="min-h-screen flex flex-col justify-center items-center "
           style={{
@@ -26,25 +53,63 @@ function App() {
             backgroundPosition: "center",
           }}
         >
-          <div className="font-semibold text-[50px] text-white mb-4">Whack a Roach</div>
-          {gameOver ? (
-          <>
-            <div className="font-bold text-red-600 text-[40px]">
-              Game Over! Final Score: {point}
-              </div>
-              <button className="text-white bg-red-600 opacity-90 text-[25px] rounded-md mt-5 p-2"> Start new Game</button> 
-          </>
+          <div className="font-semibold text-[60px] bg-gradient-to-r from-purple-950 via-purple-700 to-purple-950 bg-clip-text text-transparent mb-4">
+            Whack a Roach
+          </div>
+          {!start ? (
+            <>
+              <button
+                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-700 opacity-90 text-[25px] rounded-md mt-5 p-2"
+                onClick={startGame}
+              >
+                {" "}
+                Start Game
+              </button>
+            </>
           ) : (
-          <div
-            className="h-[480px] w-[480px] flex justify-center items-center rounded-lg"
-            style={{
-              backgroundImage: `url(${bg})`,
-              backgroundPosition: "center",
-            }}
-          >
-            <CircleGrid handleClick={handleClick} handleClickOggy = {handleClickOggy}/>
-          </div>)}
-          <div className="font-medium text-[30px] text-white mt-3"> Point Scored :{point} </div>  
+            <>
+              {gameOver ? (
+                <>
+                  <div className="font-bold text-red-600 text-[40px]">
+                    <audio src={fail} autoPlay />
+                    Game Over! Final Score: {point}
+                  </div>
+                  <button
+                    className="text-orange-50 bg-gradient-to-r from-red-600 via-red-400 to-red-600 opacity-90 text-[25px] rounded-md mt-5 p-2"
+                    onClick={startGame}
+                  >
+                    {" "}
+                    Start new Game
+                  </button>
+                </>
+              ) : (
+                <div className=" flex flex-col justify-center items-center"
+                  style={{
+                    cursor :`url(${cursor}),auto`,
+                  }}>
+                  <div
+                    className="h-[480px] w-[480px] flex justify-center items-center rounded-lg"
+                    style={{
+                      backgroundImage: `url(${bg})`,
+                      backgroundPosition: "center",
+                    }}
+                  >
+                    {" "}
+                    <audio src={audio} autoPlay loop />
+                    <audio ref={beatRef} src={beat} />
+                    <CircleGrid
+                      handleClick={handleClick}
+                      handleClickOggy={handleClickOggy}
+                    />
+                  </div>
+                  <div className="font-semibold text-[30px] text-red-950 mt-3">
+                    {" "}
+                    Point Scored : {point}
+                  </div>
+                </div>
+              )}
+            </>
+          )}
         </div>
       </div>
     </>
